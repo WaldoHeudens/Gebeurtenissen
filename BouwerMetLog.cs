b69867e29de8b223c54688d3fbb32e24ee8428f2
Waldo Heudens <waldo@heudens.be>
2021-10-11 14:58:29 +0200

    Add project files.

---
+++ BouwerMetLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gebeurtenissen
+{
+    internal class BouwerMetLog
+    {
+        public event LogEventHandler Klaar;  // Definieer de gebeurtenis Klaar
+
+        protected void AlsKlaar(string bericht)
+        {
+            if (Klaar != null)  // Als de klassegebruiker (een) methoden heeft toegevoegd aan de gebeurtenis
+                Klaar(bericht); // worden deze methoden uitgevoerd met de parameter "bericht"
+        }
+
+        internal void SchepZand(int werknemers = 1)
+        {
+            Console.WriteLine("Bezig met zand scheppen");
+            AlsKlaar("Klaar met zand scheppen");  // provoceer de gebeurtenis Klaar met de gegeven parameter
+        }
+
+        internal void PlaatsSchutting(int werknemers = 1)
+        {
+            Console.WriteLine("Bezig met het plaatsen van een schutting");
+            AlsKlaar("Klaar met schutting zetten"); // provoceer de gebeurtenis Klaar met de gegeven parameter
+        }
+
+        internal void PintjePakken(int werknemers = 1)
+        {
+            Console.WriteLine("Even een pintje drinken");
+            AlsKlaar("Pintje is op");  // provoceer de gebeurtenis Klaar met de gegeven parameter
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Gebeurtenissen;
+
+delegate void LogEventHandler(string Bericht);  // definieer het type van methode dat geassocieerd wordt met de gebeurtenis
+
+
+class Program
+{
+    // een gebeurtenismethode die voldoet aan het gedefinieerde type
+    public static void BerichtNaarOpzichter(string bericht)
+    {
+        Console.WriteLine("---> Opzichter hoorde: {0}", bericht);
+    }
+
+    // nog een gebeurtenismethode die voldoet aan het gedefinieerde type
+    public static void BerichtNaarKlant(string bericht)
+    {
+        Console.WriteLine("---> De klant kreeg bericht: {0}", bericht);
+    }
+
+    static void Main(string[] args)
+    {
+        BouwerMetLog bouwer = new BouwerMetLog();
+        object iets = new object();
+        
+        // Voeg aan de "Klaar"-gebeurtenis twee methoden toe
+        bouwer.Klaar += new LogEventHandler(BerichtNaarOpzichter);
+        bouwer.Klaar += new LogEventHandler(BerichtNaarKlant);
+
+        bouwer.SchepZand(3);
+        bouwer.PlaatsSchutting(5);
+        // Haal één van de methoden weer weg van de "Klaar"-gebeurtenis
+        bouwer.Klaar -= new LogEventHandler(BerichtNaarKlant);
+        bouwer.PintjePakken();
+    }
+}
